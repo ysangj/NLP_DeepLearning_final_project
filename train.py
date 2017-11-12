d8caa6ac3dd220d94fa0a29c60fcab12ead3d938
SJ Yum <31230614+ysangj@users.noreply.github.com>
2017-11-11 19:55:07 -0500

    4th commit

--- train.py
+++ train.py
@@ -26,10 +26,10 @@ learning_rate=0.0001
 train, val, test = datasets.IWSLT.splits(exts=('.en', '.fr'), fields=(EN, FR))
 #'<unk>': 0, '<pad>': 1, '<sos>': 2, '<eos>': 3
 
-EN.build_vocab(train.src, min_freq=300)
-FR.build_vocab(train.trg, min_freq=300)
+EN.build_vocab(train.src, min_freq=50)
+FR.build_vocab(train.trg, min_freq=50)
 
-device = 1 if(torch.cuda.is_available()) else -1
+device = 0 if(torch.cuda.is_available()) else -1
 
 train_iter, val_iter = data.BucketIterator.splits(
     (train, val), batch_size=batch_size, device=device)  #set device as any number other than -1 in cuda envicornment
@@ -40,12 +40,10 @@ decoder = DecoderRNN(hidden_size=100, output_size = len(FR.vocab))
 
 # define loss criterion
 criterion = nn.NLLLoss()
-teacher_forcing_ratio = 0.5
 
 # define optimizers
 encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
 decoder_optimizer = optim.Adam(decoder.parameters(), lr=learning_rate)
-
 
 
 def is_eos(topi, batch_size):
@@ -59,34 +57,28 @@ def is_eos(topi, batch_size):
 	return False
 
 
-def toy_train(src, trg, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
+def toy_train(src, trg, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, epoch, teacher_forcing_ratio=1.0 ):
 	loss = 0
-
 	# encode
 	encoder_hidden = encoder.init_hidden(batch_size)
 	encoder_out, context = encoder(train_batch.src, encoder_hidden)
 	#context = contexts[len(contexts)-1]
 
-	# decode
+	# decoder
 	decoder_input = Variable(torch.LongTensor([[SOS_token]*batch_size]))
-	use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
-	if use_teacher_forcing:
-		for trg_index in range(1, len(trg)):
-			decoder_output, decoder_hidden = decoder(decoder_input, context, batch_size)
-			loss += criterion(decoder_output, trg[trg_index])
-			decoder_input = trg[trg_index].view(1, len(trg[trg_index]))
-	else:
-		for trg_index in range(1, len(trg)):
-			decoder_output, decoder_hidden = decoder(decoder_input, context, batch_size)
-			#TODO: switch to sampling. For now, this is greedy
-			topv, topi = decoder_output.data.topk(1)
 
-			loss += criterion(decoder_output, trg[trg_index])
-			decoder_input  = Variable(topi.view(1, len(topi)) )
-			decoder_input = decoder_input.cuda() if torch.cuda.is_available() else decoder_input
-			# print(decoder_output)
-			if is_eos(topi, batch_size):
-				break
+	if torch.cuda.is_available():
+		decoder_input = Variable(torch.cuda.LongTensor([[SOS_token]*batch_size]))
+
+	translated = []
+	use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+	# if use_teacher_forcing:
+	for trg_index in range(1, len(trg)):
+		decoder_output, decoder_hidden = decoder(decoder_input, context, batch_size)
+		topv, topi = decoder_output.data.topk(1)
+		translated.append(topi[0][0])
+		loss += criterion(decoder_output, trg[trg_index])
+		decoder_input = trg[trg_index].view(1, len(trg[trg_index]))
 
 	decoder_optimizer.zero_grad()
 	encoder_optimizer.zero_grad()
@@ -95,17 +87,17 @@ def toy_train(src, trg, encoder, decoder, encoder_optimizer, decoder_optimizer, 
 	decoder_optimizer.step()
 	encoder_optimizer.step()
 	trglength = len(trg)
-
+	if epoch % 100 == 0:
+		print("[ENGLISH]: ", " ".join([EN.vocab.itos[i] for i in src.data[:,0]]))
+		print("[French]: ", " ".join([FR.vocab.itos[i] for i in translated]))
+		print("[French Original]: ", " ".join([FR.vocab.itos[i] for i in trg.data[:,0]]))
 	return loss.data[0]/ trglength
 
 
-
-
-def train(encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
+def train(encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, epoch, teacher_forcing_ratio=1.0):
 	train_batch = next(iter(train_iter))
 	src = train_batch.src
 	trg = train_batch.trg
-	
 	loss = 0
 
 	# encode
@@ -114,52 +106,37 @@ def train(encoder, decoder, encoder_optimizer, decoder_optimizer, criterion):
 
 	# decode
 	decoder_input = Variable(torch.LongTensor([[SOS_token]*batch_size]))
-	decoder_input = decoder_input.cuda() if torch.cuda.is_available() else decoder_input
-	use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
-	if use_teacher_forcing:
-		for trg_index in range(1, len(trg)):
-			decoder_output, decoder_hidden = decoder(decoder_input, context, batch_size)
-			loss += criterion(decoder_output, trg[trg_index])
-			decoder_input = trg[trg_index].view(1, len(trg[trg_index]))
-			#decoder_input = decoder_input.cuda() if torch.cuda.is_available() else decoder_input
-	else:
-		for trg_index in range(1, len(trg)):
-			decoder_output, decoder_hidden = decoder(decoder_input, context, batch_size)
-			#TODO: switch to sampling. For now, this is greedy
-			topv, topi = decoder_output.data.topk(1)
 
-			loss += criterion(decoder_output, trg[trg_index])
-			decoder_input  = Variable(topi.view(1, len(topi)) )
-			decoder_input = decoder_input.cuda() if torch.cuda.is_available() else decoder_input
-			# print(decoder_output)
-			if is_eos(topi, batch_size):
-				break
+	if torch.cuda.is_available():
+		decoder_input = Variable(torch.cuda.LongTensor([[SOS_token]*batch_size]))
+
+	use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+	translated = []
+
+	for trg_index in range(1, len(trg)):
+		decoder_output, decoder_hidden = decoder(decoder_input, context, batch_size)
+		topv, topi = decoder_output.data.topk(1)
+		translated.append(topi[0][0])
+		loss += criterion(decoder_output, trg[trg_index])
+		decoder_input = trg[trg_index].view(1, len(trg[trg_index]))
 
 	decoder_optimizer.zero_grad()
 	encoder_optimizer.zero_grad()
 	loss.backward()
 	decoder_optimizer.step()
 	encoder_optimizer.step()
-	
 	trglength = len(trg)
 
+	if epoch % 100 == 0:
+		print("[ENGLISH]: ", " ".join([EN.vocab.itos[i] for i in src.data[:,0]]))
+		print("[French]: ", " ".join([FR.vocab.itos[i] for i in translated]))
+		print("[French Original]: ", " ".join([FR.vocab.itos[i] for i in trg.data[:,0]]))
+
 	return loss.data[0]/ trglength
-
-
-
-
-
-
 # Toy run
 train_batch = next(iter(train_iter))
-src = train_batch.src
+src = train_batch.src # -> device 1
 trg = train_batch.trg
-teacher_forcing_ratio = 0.5
-for i in range(0, 25000):
-	print(toy_train(src, trg, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion) )
 
-
-
-
-# for i in range(0, 25000):
-#  	print(train( encoder, decoder, encoder_optimizer, decoder_optimizer, criterion) )
+for epoch in range(0, 1):
+  	print(train( encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, epoch, teacher_forcing_ratio = 1.0) )
