2a333b4ba4be7a4abb0042f0bc29225f1a8bc85a
SJ Yum <31230614+ysangj@users.noreply.github.com>
2017-11-23 18:43:15 -0500

    fixed decoder

--- model.py
+++ model.py
@@ -22,7 +22,8 @@ class EncoderRNN(nn.Module):
 
         self.embedding = nn.Embedding(input_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(input_size, hidden_size) 
         self.gru = nn.GRU(hidden_size, hidden_size, n_layers).cuda() if torch.cuda.is_available() else nn.GRU(hidden_size, hidden_size, n_layers)
-        
+        #self.lstm = nn.LSTM(hidden_size, hidden_size, n_layers).cuda() if torch.cuda.is_available() else nn.LSTM(hidden_size, hidden_size, n_layers)
+
     def forward(self, source_sentence, hidden):
         seq_length = len(source_sentence)
         embedded = self.embedding(source_sentence)
@@ -46,13 +47,13 @@ class DecoderRNN(nn.Module):
         self.out = nn.Linear(hidden_size, output_size).cuda() if torch.cuda.is_available() else nn.Linear(hidden_size, output_size)
         self.softmax = nn.LogSoftmax().cuda() if torch.cuda.is_available() else nn.LogSoftmax().cuda()
         
-    def forward(self, input, hidden, batch_size): #hidden should be renamed as context
+    def forward(self, input, hidden, context, batch_size): #hidden should be renamed as context
 
         output = self.embedding(input)
         hidden = hidden.view(self.n_layers,batch_size,self.hidden_size)
         #for i in range(self.n_layers):
         output = F.relu(output)
-        output =  torch.cat((output, hidden), 2)
+        output =  torch.cat((output, context), 2)
         output, hidden = self.gru(output, hidden)
 
         # TODO:
@@ -69,62 +70,73 @@ class DecoderRNN(nn.Module):
         return hidden
 
 
+
+
+
+
+
+
+#V3
 class RecurrentMN(nn.Module):
-    def __init__(self, hidden_size, output_size, n_layers=1, n_gram_size = 4):
+    def __init__(self, hidden_size, output_size,  memory_size, n_layers=1):
         super(RecurrentMN, self).__init__()
         self.n_layers = n_layers
         self.hidden_size = hidden_size
         self.embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size) 
         self.lstm = nn.LSTM(hidden_size*2, hidden_size,n_layers).cuda() if torch.cuda.is_available() else nn.LSTM(hidden_size*2, hidden_size, n_layers)
-        self.out = nn.Linear(hidden_size, output_size).cuda() if torch.cuda.is_available() else nn.Linear(hidden_size, output_size)
+        self.gru = nn.GRU(hidden_size*2, hidden_size).cuda() if torch.cuda.is_available() else nn.GRU(hidden_size*2, hidden_size)
+        self.out = nn.Linear(hidden_size*2, output_size).cuda() if torch.cuda.is_available() else nn.Linear(hidden_size*2, output_size)
         self.softmax = nn.LogSoftmax().cuda() if torch.cuda.is_available() else nn.LogSoftmax()
         
         ############## Memory Block ##################################################
         self.m_embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size)
         self.c_embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size)
-        self.mb_attn_linear = nn.Linear(n_gram_size, n_gram_size)
+
+        self.mb_attn_linear = nn.Linear(1, memory_size)
+
         self.mb_gru = nn.GRU(hidden_size, hidden_size).cuda() if torch.cuda.is_available() else nn.GRU(hidden_size, hidden_size)
+        self.second_lstm = nn.LSTM(hidden_size, hidden_size,n_layers).cuda() if torch.cuda.is_available() else nn.LSTM(hidden_size, hidden_size, n_layers)
         ############################################################################## 
 
 
-    def forward(self, input, hidden, batch_size, n_gram_tensor): #hidden should be renamed as context
+    def forward(self, input, rmn_hidden, cell, context, batch_size, memory_tensor, memory_size): #hidden should be renamed as context
         output = self.embedding(input)
-        hidden = hidden.view(self.n_layers,batch_size,self.hidden_size)
+        context = context.view(self.n_layers,batch_size,self.hidden_size)
         output = F.relu(output)
-        output =  torch.cat((output, hidden), 2)
-        output, (hidden,cell) = self.lstm(output, (hidden, hidden))
+        output =  torch.cat((output, context), 2)
 
+
+        lstm_output, (lstm_hidden,cell) = self.lstm(output, (rmn_hidden, cell))
+        # lstm_output, lstm_hidden = self.gru(output, rmn_hidden)
         ############## Memory Block ####################################################
-        input_memory = self.m_embedding(n_gram_tensor)
-        output_memory = self.c_embedding(n_gram_tensor)
+
+        input_memory = self.m_embedding(memory_tensor) #M 
+        output_memory = self.c_embedding(memory_tensor) #C
+
+        attn = torch.bmm(input_memory.permute(1,0,2), lstm_hidden.permute(1,2,0))# batch_size X n X 1#lstm_hidden.permute(1,2,0)
+        attn = attn.squeeze(2) #context?
+
+        # self.mb_attn_linear = nn.Linear(len(memory_tensor), len(memory_tensor))
+        # attn = self.mb_attn_linear(attn)
+        attn_dist = self.softmax(attn).unsqueeze(2)
         output_memory = output_memory.permute(1,2,0)
 
-        input_memory = input_memory.permute(1,0,2)
-        hidden = hidden.permute(1,2,0)
+        memory_context = torch.bmm(output_memory,  attn_dist).permute(2,0,1)
 
-        attn = torch.bmm(input_memory, hidden)# batch_size X n X 1
-        attn = attn.squeeze(2)
-
-        lin = self.mb_attn_linear(attn)
-        attn_dist = self.softmax(lin)
-        attn_dist = attn_dist.view(batch_size, len(n_gram_tensor), -1)
-
-        context = torch.bmm(output_memory, attn_dist).view(-1, batch_size, self.hidden_size)
-        hidden = hidden.view(-1, batch_size, self.hidden_size)
-
-        mb_output, mb_hidden = self.mb_gru(context,hidden)
-        print(mb_output.size())
-        print(output.size())
-
-        #return mb_output
+        mb_hidden, mb_output = self.mb_gru(memory_context, lstm_hidden)
+        # mb_output, mb_hidden = self.mb_gru(memory_context, lstm_hidden)
         output = mb_output
-
-        #s should be batch X hidden_size X 1
-        ####################################################################################        
-
+        ####################################################################################
+        
+        output = torch.cat((output, context),2)
         output = self.softmax(self.out(output[0]))
+        # output = self.softmax(self.out(output[0]))
 
-        return output, hidden
+
+        # return output, hidden
+        return output, lstm_hidden, cell
+
+
 
     def init_hidden(self, batch_size):
         hidden = Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size))
@@ -132,6 +144,173 @@ class RecurrentMN(nn.Module):
         if torch.cuda.is_available():
             hidden = Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size).cuda())
         return hidden
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#V2
+# class RecurrentMN(nn.Module):
+#     def __init__(self, hidden_size, output_size,  memory_size, n_layers=1):
+#         super(RecurrentMN, self).__init__()
+#         self.n_layers = n_layers
+#         self.hidden_size = hidden_size
+#         self.embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size) 
+#         self.lstm = nn.LSTM(hidden_size, hidden_size,n_layers).cuda() if torch.cuda.is_available() else nn.LSTM(hidden_size, hidden_size, n_layers)
+#         self.gru = nn.GRU(hidden_size*2, hidden_size).cuda() if torch.cuda.is_available() else nn.GRU(hidden_size*2, hidden_size)
+#         self.out = nn.Linear(hidden_size*2, output_size).cuda() if torch.cuda.is_available() else nn.Linear(hidden_size*2, output_size)
+#         self.softmax = nn.LogSoftmax().cuda() if torch.cuda.is_available() else nn.LogSoftmax()
+        
+#         ############## Memory Block ##################################################
+#         self.m_embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size)
+#         self.c_embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size)
+
+#         self.mb_attn_linear = nn.Linear(1, memory_size)
+
+#         self.mb_gru = nn.GRU(hidden_size, hidden_size).cuda() if torch.cuda.is_available() else nn.GRU(hidden_size , hidden_size)
+#         self.second_lstm = nn.LSTM(hidden_size, hidden_size,n_layers).cuda() if torch.cuda.is_available() else nn.LSTM(hidden_size, hidden_size, n_layers)
+#         ############################################################################## 
+
+
+#     def forward(self, input, context, batch_size, memory_tensor, memory_size): #hidden should be renamed as context
+
+#         ############## Memory Block ####################################################
+
+#         input_memory = self.m_embedding(memory_tensor) #M 
+#         output_memory = self.c_embedding(memory_tensor) #C
+
+#         attn = torch.bmm(input_memory.permute(1,0,2), context.permute(1,2,0))# batch_size X n X 1
+#         attn = attn.squeeze(2)
+
+#         # self.mb_attn_linear = nn.Linear(len(memory_tensor), len(memory_tensor))
+#         # attn = self.mb_attn_linear(attn)
+#         attn_dist = self.softmax(attn).unsqueeze(2)
+#         output_memory = output_memory.permute(1,2,0)
+
+#         memory_context = torch.bmm(output_memory, attn_dist).permute(2,0,1) #S
+
+#         mb_output, mb_hidden = self.mb_gru(memory_context, context)
+#         output = mb_hidden
+
+#         ####################################################################################
+#         output = torch.cat((output, context),2)
+#         output = self.softmax(self.out(output[0]))
+
+#         return output, context
+        
+
+
+#     def init_hidden(self, batch_size):
+#         hidden = Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size))
+
+#         if torch.cuda.is_available():
+#             hidden = Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size).cuda())
+#         return hidden
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#V1
+# class RecurrentMN(nn.Module):
+#     def __init__(self, hidden_size, output_size,  memory_size, n_layers=1):
+#         super(RecurrentMN, self).__init__()
+#         self.n_layers = n_layers
+#         self.hidden_size = hidden_size
+#         self.embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size) 
+#         self.lstm = nn.LSTM(hidden_size*2, hidden_size,n_layers).cuda() if torch.cuda.is_available() else nn.LSTM(hidden_size*2, hidden_size, n_layers)
+#         self.gru = nn.GRU(hidden_size*2, hidden_size).cuda() if torch.cuda.is_available() else nn.GRU(hidden_size*2, hidden_size)
+#         self.out = nn.Linear(hidden_size, output_size).cuda() if torch.cuda.is_available() else nn.Linear(hidden_size, output_size)
+#         self.softmax = nn.LogSoftmax().cuda() if torch.cuda.is_available() else nn.LogSoftmax()
+        
+#         ############## Memory Block ##################################################
+#         self.m_embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size)
+#         self.c_embedding = nn.Embedding(output_size, hidden_size).cuda() if torch.cuda.is_available() else nn.Embedding(output_size, hidden_size)
+
+#         self.mb_attn_linear = nn.Linear(1, memory_size)
+
+#         self.mb_gru = nn.GRU(hidden_size, hidden_size).cuda() if torch.cuda.is_available() else nn.GRU(hidden_size, hidden_size)
+#         self.second_lstm = nn.LSTM(hidden_size, hidden_size,n_layers).cuda() if torch.cuda.is_available() else nn.LSTM(hidden_size, hidden_size, n_layers)
+#         ############################################################################## 
+
+
+#     def forward(self, input, context, batch_size, memory_tensor, memory_size): #hidden should be renamed as context
+#         output = self.embedding(input)
+#         context = context.view(self.n_layers,batch_size,self.hidden_size)
+#         output = F.relu(output)
+#         output =  torch.cat((output, context), 2)
+
+#         cell = Variable(torch.zeros(1, batch_size, self.hidden_size))
+#         lstm_output, (lstm_hidden, cell) = self.lstm(output, (context, cell))
+#         #lstm_output, lstm_hidden = self.gru(output, context)
+#         ############## Memory Block ####################################################
+
+#         input_memory = self.m_embedding(memory_tensor) #M 
+#         output_memory = self.c_embedding(memory_tensor) #C
+
+#         attn = torch.bmm(input_memory.permute(1,0,2), lstm_hidden.permute(1,2,0))# batch_size X n X 1
+#         attn = attn.squeeze(2)
+
+#         self.mb_attn_linear = nn.Linear(len(memory_tensor), len(memory_tensor))
+#         attn = self.mb_attn_linear(attn)
+#         attn_dist = self.softmax(attn).unsqueeze(2)
+#         output_memory = output_memory.permute(1,2,0)
+
+#         memory_context = torch.bmm(output_memory,  attn_dist).permute(2,0,1)
+
+#         #_, mb_output = self.mb_gru(memory_context, context)
+#         mb_output = memory_context + lstm_hidden
+
+#         output = mb_output
+#         ####################################################################################
+
+#         output = self.softmax(self.out(output[0]))
+#         # output = self.softmax(self.out(output[0]))
+
+
+#         # return output, hidden
+#         return output, lstm_hidden
+
+
+
+#     def init_hidden(self, batch_size):
+#         hidden = Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size))
+
+#         if torch.cuda.is_available():
+#             hidden = Variable(torch.zeros(self.n_layers, batch_size, self.hidden_size).cuda())
+#         return hidden
 
 
 
