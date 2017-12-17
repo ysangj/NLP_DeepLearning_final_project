26b7632043a6c517db1d822a6c0fabdd0ffdaa22
SJ Yum <31230614+ysangj@users.noreply.github.com>
2017-12-17 13:23:49 -0500

    FINAL PUSH

--- train_RMN_modified.py
+++ train_RMN_modified.py
@@ -15,7 +15,7 @@ import numpy as np
 from torchtext import data
 from torchtext import datasets
 from model import EncoderRNN, RecurrentMemory
-
+import queue
 import nltk
 from collections import deque
 
@@ -23,29 +23,40 @@ from six.moves import urllib
 import os
 import subprocess
 import logging
+import spacy
+
+spacy_de = spacy.load('de')
+spacy_en = spacy.load('en')
+
+url = re.compile('(<url>.*</url>)')
+def tokenize_de(text):
+    return [tok.text for tok in spacy_de.tokenizer(url.sub('@URL@', text))]
+
+def tokenize_en(text):
+    return [tok.text for tok in spacy_en.tokenizer(url.sub('@URL@', text))]
 
 multi_bleu_path, _ = urllib.request.urlretrieve(
         "https://raw.githubusercontent.com/moses-smt/mosesdecoder/"
         "master/scripts/generic/multi-bleu.perl")
-
 os.chmod(multi_bleu_path, 0o755)
-file_name = 'dec7_IWSLT_1'
+
+file_name = 'dec15_rmn_iwslt_3_en_de'
 logging.basicConfig(filename=file_name+'.log',level=logging.WARNING)
 logging.warning('Packages Imported')
 
-DE = data.Field(init_token='<sos>', eos_token='<eos>')
-EN = data.Field(init_token='<sos>', eos_token='<eos>')
+DE = data.Field(tokenize=tokenize_de, init_token='<sos>', eos_token='<eos>', fix_length = 13)
+EN = data.Field(tokenize=tokenize_en, init_token='<sos>', eos_token='<eos>', fix_length = 13)
 
 UNK_token = 0
 PAD_token = 1
 SOS_token = 2
 EOS_token = 3
 #'<unk>': 0, '<pad>': 1, '<sos>': 2, '<eos>': 3
-
 train_set, val_set, test_set = datasets.IWSLT.splits(exts=('.en', '.de'), fields=(EN, DE))
 
 device = 0 if(torch.cuda.is_available()) else -1
 logging.warning('Data Loaded')
+logging.warning('Embedding Size = 128, Fix Length = 13')
 def queue2tensor(memory_queue):
 	tensor = memory_queue[0]
 	for i in range(1,len(memory_queue)):
@@ -57,6 +68,7 @@ def is_eos(topi, batch_size):
 	for i in range(0, batch_size):
 		if topi[i][0] == 3 or topi[i][0] == 1 :
 			eos_counter += 1
+
 	if eos_counter == batch_size:
 		return True
 	return False
@@ -74,6 +86,7 @@ def compute_bleu(hypothesis_file_name, test_filename_name):
 	        bleu_out = bleu_out.decode("utf-8")
 	        bleu_score = re.search(r"BLEU = (.+?),", bleu_out).group(1)
 	        bleu_score = float(bleu_score)
+
 	    except subprocess.CalledProcessError as error:
 	        if error.output is not None:
 	            logging.warning("multi-bleu.perl script returned non-zero exit code")
@@ -92,6 +105,7 @@ def rm_train(train_iter, encoder, recurrent_memory, encoder_optimizer, recurrent
 
 		encoder_hidden = encoder.init_hidden(train_iter.batch_size)
 		encoder_out, context = encoder(src, encoder_hidden)
+		context = context[-1].unsqueeze(0)
 
 		# decoder
 		recurrent_memory_input = Variable(torch.LongTensor([[SOS_token]*train_iter.batch_size]))
@@ -113,9 +127,7 @@ def rm_train(train_iter, encoder, recurrent_memory, encoder_optimizer, recurrent
 		for trg_index in range(1, len(trg)):
 			memory_tensor = queue2tensor(memory_queue)
 			recurrent_memory_output, recurrent_memory_hidden, cell = recurrent_memory(recurrent_memory_input, recurrent_memory_hidden, cell, context, train_iter.batch_size, memory_tensor, memory_size)
-			
 			topv, topi = recurrent_memory_output.data.topk(1)
-
 			loss += criterion(recurrent_memory_output, trg[trg_index])
 			recurrent_memory_input = trg[trg_index].view(1, len(trg[trg_index]))
 			memory_queue.append(trg[trg_index].view(1, len(trg[trg_index])))
@@ -153,6 +165,7 @@ def evaluate(val_iter, encoder, recurrent_memory, hidden_size, memory_size, crit
 				# encode
 		encoder_hidden = encoder.init_hidden(val_iter.batch_size)
 		encoder_out, context = encoder(src, encoder_hidden)
+		context = context[-1].unsqueeze(0)
 
 		# decode
 		recurrent_memory_input = Variable(torch.LongTensor([[SOS_token]*val_iter.batch_size]))
@@ -198,7 +211,6 @@ def evaluate(val_iter, encoder, recurrent_memory, hidden_size, memory_size, crit
 			print("[ENGLISH]: ", " ".join([EN.vocab.itos[i] for i in src.data[:,0]]))
 			print("[DE]: ", " ".join([DE.vocab.itos[i] for i in translated]))
 			print("[DE Original]: ", " ".join([DE.vocab.itos[i] for i in trg.data[:,0]]))
-
 		if b==len(val_iter)-1:
 			break
 
@@ -206,7 +218,7 @@ def evaluate(val_iter, encoder, recurrent_memory, hidden_size, memory_size, crit
 	for j in range(len(de_test)):
 	    sentence1 += ' '.join(de_test[j][i] for i in range(len(de_test[j])))
 	    sentence1 = sentence1+'\n'
-	text_file = open("DE_val_test_"+file_name+".txt", "w")
+	text_file = open("de_val_test_"+file_name+".txt", "w")
 	text_file.write(sentence1)
 	text_file.close()
 
@@ -214,21 +226,21 @@ def evaluate(val_iter, encoder, recurrent_memory, hidden_size, memory_size, crit
 	for j in range(len(de_hypo)):
 	    sentence2 += ' '.join(de_hypo[j][i] for i in range(len(de_hypo[j])))
 	    sentence2 = sentence2+'\n'
-	text_file = open("DE_val_hypo_"+file_name+".txt", "w")
+	text_file = open("de_val_hypo_"+file_name+".txt", "w")
 	text_file.write(sentence2)
 	text_file.close()
 
-	val_bleu = compute_bleu("DE_val_hypo_"+file_name+".txt", "DE_val_test_"+file_name+".txt")[0]
+	val_bleu = compute_bleu("de_val_hypo_"+file_name+".txt", "de_val_test_"+file_name+".txt")[0]
 	return total_loss/len(val_iter), val_bleu
 
-def epoch_training(train_iter, val_iter, num_epoch = 100, learning_rate = 1e-4, hidden_size = 100, patience = 4, epsilon = 1e-4, memory_size = 7):
+def epoch_training(train_iter, val_iter, num_epoch = 100, learning_rate = 1e-4, hidden_size = 1000,  early_stop = False, patience = 4, epsilon = 1e-4, memory_size = 7):
 
     # define model
     encoder = EncoderRNN(input_size = len(EN.vocab), hidden_size = hidden_size)
     recurrent_memory = RecurrentMemory(hidden_size=hidden_size, output_size = len(DE.vocab), memory_size = memory_size)
 
     # define loss criterion
-    criterion = nn.NLLLoss()
+    criterion = nn.NLLLoss( ignore_index = PAD_token)
 
     # define optimizers
     encoder_optimizer = optim.Adam(encoder.parameters(), lr=learning_rate)
@@ -240,8 +252,7 @@ def epoch_training(train_iter, val_iter, num_epoch = 100, learning_rate = 1e-4, 
     res_rm = None
     res_epoch = 0
     base_bleu = 0
-    not_updated = 0
-    
+
     for epoch in range(num_epoch):
         tl = rm_train(train_iter, encoder, recurrent_memory, encoder_optimizer, recurrent_memory_optimizer, criterion, hidden_size, memory_size )
         loss, val_bleu = evaluate(val_iter, encoder, recurrent_memory, hidden_size, memory_size, criterion)
@@ -258,14 +269,11 @@ def epoch_training(train_iter, val_iter, num_epoch = 100, learning_rate = 1e-4, 
         else:
         	not_updated += 1
         if not_updated == patience:
-                break
+			break
     print('Stop at Epoch: '+str(res_epoch)+", With Validation Loss: "+str(res_loss)+' Validation Bleu: '+str(base_bleu))
     logging.warning('Stop at Epoch: '+str(epoch)+", With Validation Loss: "+str(loss)+' Validation Bleu: '+str(base_bleu))
     return res_loss, res_encoder, res_rm, base_bleu
 
-'''
-Compute NLTK Bleu score and return translation hypothesis and reference
-'''
 def test(encoder, recurrent_memory, device, test_set, hidden_size, memory_size):
 	test_iter, = data.BucketIterator.splits((test_set,), batch_sizes=(1,), device=device)
 	avg_bleu1 = 0
@@ -284,6 +292,7 @@ def test(encoder, recurrent_memory, device, test_set, hidden_size, memory_size):
 
 		encoder_hidden = encoder.init_hidden(test_iter.batch_size)
 		encoder_out, context = encoder(src, encoder_hidden)
+		context = context[-1].unsqueeze(0)
 
 		recurrent_memory_input = Variable(torch.cuda.LongTensor([[SOS_token]*test_iter.batch_size])) if torch.cuda.is_available() else Variable(torch.LongTensor([[SOS_token]*test_iter.batch_size]))
 		recurrent_memory_hidden = context
@@ -301,9 +310,7 @@ def test(encoder, recurrent_memory, device, test_set, hidden_size, memory_size):
 
 		for trg_index in range(1, len(trg)):
 			memory_tensor = queue2tensor(memory_queue)
-
 			recurrent_memory_output, recurrent_memory_hidden, cell = recurrent_memory(recurrent_memory_input, recurrent_memory_hidden, cell, context, test_iter.batch_size, memory_tensor, memory_size)
-			
 			topv, topi = recurrent_memory_output.data.topk(1)
 			translated.append(topi[0][0])
 			recurrent_memory_input = Variable(topi.view(1, len(topi)) )
@@ -345,21 +352,16 @@ def test(encoder, recurrent_memory, device, test_set, hidden_size, memory_size):
 
 	return de_test, de_hypo, english_test, [avg_bleu1, avg_bleu2, avg_bleu3, avg_bleu4, avg_bleu5, avg_bleu7]
 
+############################################################### MAIN PROCEDURE ########################################################################## 
 
-
-
-
-
-
-#################################################### MAIN PROCEDURE ########################################################################## 
 
 ############################################################### Train/Validate Model and do Model Selection via Random Search ###################################
 pars = []
 for num_epoch in [50]:
-    for learning_rate in [3e-4]:#,0.05]:
-        for hidden_size in [128]:
+    for learning_rate in [1e-4]:#,0.05]:
+        for hidden_size in [1000]:
             for batch_size in [12]:
-                for voc_size in [30000]:
+                for voc_size in [58000]:
                         for memory_size in [7]:
                             pars.append({
                                 'num_epoch': num_epoch,
@@ -381,11 +383,11 @@ while cnt <1 :
 	np.random.seed()
 	par = np.random.choice(pars, 1)[0]
 	logging.warning(str(cnt) +'th trial: \n Parameters: '+ str(par))
-	EN.build_vocab(train_set.src, max_size=par['voc_size'])
+	EN.build_vocab(train_set.src, min_freq=2)
 	DE.build_vocab(train_set.trg, max_size=par['voc_size'])
-	# train_iter, val_iter, = data.BucketIterator.splits((train_set, val_set,), batch_sizes=(4, 1,), device = device)
+
 	train_iter, val_iter = data.BucketIterator.splits((train_set, val_set), sort_key = lambda ex: data.interleave_keys(len(ex.src), len(ex.trg)), batch_sizes=(par['batch_size'], 1), device = device)
-	loss, encoder, recurrent_memory,bleu = epoch_training(train_iter, val_iter, num_epoch = par['num_epoch'], learning_rate = par['learning_rate'], hidden_size = par['hidden_size'], patience = 15, epsilon = 1e-4, memory_size = par['memory_size'])
+	loss, encoder, recurrent_memory,bleu = epoch_training(train_iter, val_iter, num_epoch = par['num_epoch'], learning_rate = par['learning_rate'], hidden_size = par['hidden_size'], early_stop = False, patience = 5, epsilon = 1e-4, memory_size = par['memory_size'])
 	logging.warning('\nValidation Loss: '+str(loss)+' Validation Bleu: '+str(bleu))
 
 	if loss < base_loss:
@@ -398,8 +400,6 @@ while cnt <1 :
 
 gc.collect()
 logging.warning('Optimized Parameters are ' + str(optimized_parameters))
-pickle.dump(encoder_model,open('encoder_'+file_name+'.p','wb'))
-pickle.dump(recurrent_memory_model,open('recurrent_memory_'+file_name+'.p','wb'))
 
 ############################################### Test Model by computing BLEU score ###########################
 de_reference, de_hypo, english_test, nltk_bleus = test(encoder_model, recurrent_memory_model, device, test_set, optimized_parameters['hidden_size'], optimized_parameters['memory_size'] )
@@ -418,7 +418,7 @@ reference = ''
 for j in range(len(de_reference)):
     reference += ' '.join(de_reference[j][i] for i in range(len(de_reference[j])))
     reference = reference + '\n'
-text_file = open("DE_reference_"+file_name+".txt", "w")
+text_file = open("de_reference_"+file_name+".txt", "w")
 text_file.write(reference)
 text_file.close()
 
@@ -426,11 +426,14 @@ hypothesis = ''
 for j in range(len(de_hypo)):
     hypothesis += ' '.join(de_hypo[j][i] for i in range(len(de_hypo[j])))
     hypothesis = hypothesis + '\n'
-text_file = open("DE_hypo_"+file_name+".txt", "w")
+text_file = open("de_hypo_"+file_name+".txt", "w")
 text_file.write(hypothesis)
 text_file.close()
 
-bleu_score,bleu_out = compute_bleu("DE_hypo_"+file_name+".txt", "DE_reference_"+file_name+".txt")
+bleu_score,bleu_out = compute_bleu("de_hypo_"+file_name+".txt", "de_reference_"+file_name+".txt")
 
 logging.warning(str(bleu_score))
 logging.warning(bleu_out)
+
+pickle.dump(encoder_model,open('/scratch/sjy269/encoder_'+file_name+'.p','wb'))
+pickle.dump(recurrent_memory_model,open('/scratch/sjy269/recurrent_memory_'+file_name+'.p','wb'))
